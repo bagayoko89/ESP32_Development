db193e5b3754a90918abc35604f6466f65b3ed63
bagayoko89 <bagayokoname@gmail.com>
2025-08-20 20:41:06 +0200

    add timer and freertos librairies

--- ESP32_Software_Timer/ESP32_Software_Timer/ESP32_Software_Timer.ino
+++ ESP32_Software_Timer/ESP32_Software_Timer/ESP32_Software_Timer.ino
@@ -1,15 +1,21 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/list.h"
+#include <freertos/semphr.h>
+TimerHandle_t blink_timer;
+#define buzzer_pin 32
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(32,OUTPUT);
+  pinMode(buzzer_pin,OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(32,HIGH);
+  digitalWrite(buzzer_pin,HIGH);
   delay(3000);
-  digitalWrite(32,LOW);
+  digitalWrite(buzzer_pin,LOW);
   delay(2000);
 
 }

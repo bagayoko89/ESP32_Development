8b97d327f9b0d200fc3310eb514689dd8c65cf3d
bagayoko89 <bagayokoname@gmail.com>
2025-08-26 16:13:20 +0200

    turn ON a led if the buzzer is ON else turn OFF the led

--- ESP32_Software_Timer/ESP32_Software_Timer/ESP32_Software_Timer.ino
+++ ESP32_Software_Timer/ESP32_Software_Timer/ESP32_Software_Timer.ino
@@ -1,15 +1,23 @@
 #include "esp_timer.h"
 #define buzzer_pin 32
+#define led_pin 33
 esp_timer_handle_t my_timer;
+static bool buzzerState = false;
 void toggleBuzzer(){
-  static bool buzzerState = false;
   buzzerState = !buzzerState;
   digitalWrite(buzzer_pin, buzzerState);
+}
+void toggleLED(){
+  if(buzzerState){
+    digitalWrite(led_pin, HIGH);
+  }
+  else digitalWrite(led_pin, LOW);
 }
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(buzzer_pin,OUTPUT);
+  pinMode(led_pin,OUTPUT);
 
   const esp_timer_create_args_t timer_args = {
     .callback = &toggleBuzzer,   
@@ -26,5 +34,5 @@ void setup() {
 }
 
 void loop() {
-
+  toggleLED();
 }

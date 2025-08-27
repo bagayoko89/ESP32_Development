744bfbe9126f6625baffb7d2555cc1103ba90b8f
bagayoko89 <bagayokoname@gmail.com>
2025-08-27 21:19:18 +0200

    use esp-idf in visual code for esp32 project

---
+++ FreeRTOS ESP32/src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+void setup() {
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}

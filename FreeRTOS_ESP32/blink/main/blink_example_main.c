9094ad54ebe81f539ca120000fd20dc214ba4442
bagayoko89 <bagayokoname@gmail.com>
2025-09-01 17:33:43 +0200

    compare vTaskDelay and vTaskDelayUntil performances

--- FreeRTOS_ESP32/blink/main/blink_example_main.c
+++ FreeRTOS_ESP32/blink/main/blink_example_main.c
@@ -5,38 +5,24 @@
 
 #define LED_PIN 33
 
-void task_low(void *pvParameters){
+void task_delay(void *pvParameters){
     while(1){
-        printf("Low priority task running on core %d\n",xPortGetCoreID());
+        printf("vTaskDelay: %lu ms\n", (unsigned long)(xTaskGetTickCount()* portTICK_PERIOD_MS));
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
-void task_medium(void *pvParameters3){
+void task_delay_until(void *pvParameters3){
+    TickType_t last_wake= xTaskGetTickCount();
     while(1){
-        printf("Medium priority task running on core %d\n",xPortGetCoreID());
-        vTaskDelay(pdMS_TO_TICKS(500));
-    }
-}
-
-void task_high(void *pvParameters3){
-    int i=0;
-    while(1){
-        printf("High priority task running on core %d and count=%d\n",xPortGetCoreID(), i);
-        vTaskDelay(pdMS_TO_TICKS(1000));
-        if(i>=4){
-            printf("Priority changed\n");
-            vTaskPrioritySet(NULL, 2);
-            i=0;
-        }
-        i++;
+        printf("vTaskDelayUntil: %lu ms\n", (unsigned long)(xTaskGetTickCount()* portTICK_PERIOD_MS));
+        vTaskDelayUntil(&last_wake,pdMS_TO_TICKS(1000));
     }
 }
 
 
 void app_main(void)
 {
-    xTaskCreate(task_low, "LowPriority",2048,NULL,3,NULL);
-    xTaskCreate(task_medium, "MediumPriority",2048,NULL,5,NULL);
-    xTaskCreate(task_high, "HighPriority",2048,NULL,8,NULL);
+    xTaskCreate(task_delay, "TaskDelay",2048,NULL,5,NULL);
+    xTaskCreate(task_delay_until, "TaskDelayUntil",2048,NULL,5,NULL);
 }
